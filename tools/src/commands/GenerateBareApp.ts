2cb53fa1c07e9bd792f3a6ab28b3de727d4a3bea
andy <40680668+ajsmth@users.noreply.github.com>
2022-03-02 11:24:07 -0800

    [expotool] generate bare app (iOS)

---
+++ tools/src/commands/GenerateBareApp.ts
@@ -0,0 +1,133 @@
+import path from 'path';
+import fs from 'fs-extra';
+import { Command } from '@expo/commander';
+import spawnAsync from '@expo/spawn-async';
+import { runExpoCliAsync } from '../ExpoCLI';
+
+type GenerateBareAppOptions = {
+  template: string;
+  clean?: boolean;
+  outDir?: string;
+};
+
+async function action(appName: string, packageNames: string[], options: GenerateBareAppOptions) {
+  function symlinkPackageManually(packageName: string) {
+    const packagePath = path.resolve(projectDir, 'node_modules', packageName);
+    if (fs.existsSync(packagePath)) {
+      fs.rmdirSync(path.resolve(projectDir, 'node_modules', packageName), { recursive: true });
+    }
+
+    fs.symlinkSync(path.resolve(repoRoot, 'packages', packageName), packagePath);
+  }
+
+  // TODO:
+  // if appName === ''
+  // if packageNames.length === 0
+  // configure autolinking to exclude dev-client / expo update packages
+
+  const { clean, outDir = 'bare-apps', template = 'expo-template-bare-minimum' } = options;
+
+  const repoRoot = path.resolve(__dirname, '../../../');
+  const projectsDir = path.resolve(process.cwd(), outDir);
+  const projectDir = path.resolve(process.cwd(), projectsDir, appName);
+
+  if (!fs.existsSync(projectsDir)) {
+    fs.mkdirSync(projectsDir);
+  }
+
+  if (clean) {
+    if (fs.existsSync(projectDir)) {
+      fs.rmdirSync(projectDir, { recursive: true });
+    }
+  }
+
+  // these don't seem to symlink properly w/ expo-yarn-workspaces
+  const symlinkedPackages = ['expo', 'expo-modules-autolinking'];
+
+  // these seem to be included regardless of what is in node_modules (default packages for prebuild)
+  const defaultPackages = [
+    'expo-asset',
+    'expo-application',
+    'expo-constants',
+    'expo-file-system',
+    'expo-font',
+    'expo-keep-awake',
+    'expo-error-recovery',
+    'expo-modules-core',
+    'expo-splash-screen',
+    'expo-updates',
+    'expo-dev-client',
+    'expo-dev-launcher',
+    'expo-dev-menu',
+    'expo-dev-menu-interface',
+  ];
+
+  await runExpoCliAsync('init', [appName, '--no-install', '--template', template], {
+    cwd: projectsDir,
+  });
+
+  const pkg = require(path.resolve(projectDir, 'package.json'));
+
+  pkg['expo'] = pkg['expo'] ?? {};
+
+  // TODO - autolinking -> { include?: [] }
+  // pkg['expo']['autolinking'] = { exclude: [] };
+
+  pkg['scripts']['postinstall'] = 'expo-yarn-workspaces postinstall';
+  pkg['main'] = '__generated__/AppEntry.js';
+
+  pkg['expo-yarn-workspaces'] = {};
+  pkg['expo-yarn-workspaces']['symlinks'] = [];
+
+  packageNames.forEach((packageName) => {
+    pkg.dependencies[packageName] = '*';
+    pkg['expo-yarn-workspaces']['symlinks'].push(packageName);
+  });
+
+  defaultPackages.forEach((packageName) => {
+    pkg.dependencies[packageName] = '*';
+    pkg['expo-yarn-workspaces']['symlinks'].push(packageName);
+  });
+
+  fs.writeJsonSync(path.resolve(projectDir, 'package.json'), pkg);
+
+  console.log('Yarning');
+  await spawnAsync('yarn', [], { cwd: projectDir });
+
+  symlinkedPackages.forEach((packageName) => {
+    symlinkPackageManually(packageName);
+  });
+
+  await runExpoCliAsync('prebuild', ['--clean'], { cwd: projectDir });
+
+  const ncl = path.resolve(repoRoot, 'apps/native-component-list');
+
+  fs.copyFileSync(
+    path.resolve(ncl, 'metro.config.js'),
+    path.resolve(projectDir, 'metro.config.js')
+  );
+
+  fs.copyFileSync(
+    path.resolve(ncl, 'metro.transformer.js'),
+    path.resolve(projectDir, 'metro.transformer.js')
+  );
+
+  // for some reason prebuild is updating the gradle.properties FLIPPER_VERSION which causes SoLoader crash on launch
+  const gradlePropertiesPath = path.resolve(projectDir, 'android', 'gradle.properties');
+  const gradleProperties = fs.readFileSync(gradlePropertiesPath, { encoding: 'utf-8' });
+  const updatedGradleProperies = gradleProperties.replace(
+    `FLIPPER_VERSION=0.54.0`,
+    `FLIPPER_VERSION=0.99.0`
+  );
+  fs.writeFileSync(gradlePropertiesPath, updatedGradleProperies);
+}
+
+export default (program: Command) => {
+  program
+    .command('generate-bare-app [appName] [packageNames...]')
+    .option('-c, --clean', 'Rebuild [appName] from scratch')
+    .option('-o, --outDir <string>', 'Specifies the directory to build the project in')
+    .option('-t, --template <string>', 'Specify the expo template to use w/ the project')
+    .description(`Generates a bare app with the specified packages symlinked`)
+    .asyncAction(action);
+};
